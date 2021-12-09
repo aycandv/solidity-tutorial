a1b594fdbe49cf3ff5e53d05c94fa964265f5154
Aycan Deniz Vit <53761495+aycandv@users.noreply.github.com>
2021-12-09 22:57:02 +0300

    style: add empty lines

--- simple-storage-factory/SimpleStorage.sol
+++ simple-storage-factory/SimpleStorage.sol
@@ -7,15 +7,19 @@ contract SimpleStorage {
         string name;
         uint256 favoriteNumber;
     }
+    
     Person[] public people;
     mapping(string => uint256) public nameToFavoriteNumber;
+    
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
+    
     // If you read from contract, use view
     function retrieve() public view returns(uint256) {
         return favoriteNumber;
     }
+    
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(Person({name: _name, favoriteNumber: _favoriteNumber}));
         nameToFavoriteNumber[_name] = _favoriteNumber;
